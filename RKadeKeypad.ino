959f7841092f7da37bc44504b7fd63df454e0b2d
rknabe <rusty.knabe@gmail.com>
2024-11-12 23:11:59 -0600

    support key combo's

--- RKadeKeypad.ino
+++ RKadeKeypad.ino
@@ -1,106 +1,120 @@
-// Use this example with the Adafruit Keypad products.
-// You'll need to know the Product ID for your keypad.
-// Here's a summary:
-//   * PID3844 4x4 Matrix Keypad
-//   * PID3845 3x4 Matrix Keypad
-//   * PID1824 3x4 Phone-style Matrix Keypad
-//   * PID1332 Membrane 1x4 Keypad
-//   * PID419  Membrane 3x4 Matrix Keypad
-
-#include "Adafruit_Keypad.h"
+#include <Keypad.h>
 #include "Keyboard.h"
 
-// define your specific keypad here via PID
-#define KEYPAD_PID1824
-// define your pins here
-// can ignore ones that don't apply
-#define R1 2
-#define R2 3
-#define R3 4
-#define R4 5
-#define C1 8
-#define C2 9
-#define C3 10
-//e#define C4    11  // not needed for 3x4
-// leave this import after the above configuration
-#include "keypad_config.h"
+const byte ROWS = 4;  //four rows
+const byte COLS = 3;  //three columns
+char keys[ROWS][COLS] = {
+  { '1', '2', '3' },
+  { '4', '5', '6' },
+  { '7', '8', '9' },
+  { '*', '0', '#' }
+};
+byte rowPins[ROWS] = { 5, 4, 3, 2 };  //connect to the row pinouts of the kpd
+byte colPins[COLS] = { 8, 7, 6 };     //connect to the column pinouts of the kpd
+//byte rowPins[ROWS] = { 2, 3, 4, 5 };  //connect to the row pinouts of the kpd
+//byte colPins[COLS] = { 8, 9, 10 };    //connect to the column pinouts of the kpd
 
-//initialize an instance of class NewKeypad
-Adafruit_Keypad keypad = Adafruit_Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-uint8_t lastKeyPressed = 0;
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
   Serial.begin(9600);
   Keyboard.begin();
-  keypad.begin();
+}
+
+bool isHeld(char keyChar) {
+  for (byte i = 0; i < LIST_MAX; i++) {
+    if (keypad.key[i].kchar == keyChar) {
+      if ((keypad.key[i].kstate == HOLD))
+        return true;
+    }
+  }
+  return false;  // Not pressed.
+}
+
+bool isPressed(char keyChar) {
+  for (byte i = 0; i < LIST_MAX; i++) {
+    if (keypad.key[i].kchar == keyChar) {
+      if ((keypad.key[i].kstate == PRESSED))
+        return true;
+    }
+  }
+  return false;  // Not pressed.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  keypad.tick();
 
-  while (keypad.available()) {
-    keypadEvent e = keypad.read();
-    uint8_t key = 0;
-    switch (e.bit.KEY) {
-      case '*':
-        key = 221;
-        break;
-      case '#':
-        if (keypad.isPressed('*')) {
-          Serial.println("combo");
-          key =	224;
-        } else {
-          key = 235;
-          Serial.println("not combo");
+  // Fills keypad.key[ ] array with up-to 10 active keys.
+  // Returns true if there are ANY active keys.
+  if (keypad.getKeys()) {
+    for (int i = 0; i < LIST_MAX; i++) {  // Scan the whole key list.
+      if (keypad.key[i].stateChanged) {   // Only find keys that have changed state.
+        char key = keypad.key[i].kchar;
+        char keycode = 0;
+        switch (key) {
+          case '*':
+            keycode = 221;
+            break;
+          case '#':
+            keycode = 235;
+            break;
+          case '1':
+            keycode = 225;
+            break;
+          case '2':
+            keycode = 226;
+            break;
+          case '3':
+            keycode = 227;
+            break;
+          case '4':
+            keycode = 228;
+            break;
+          case '5':
+            keycode = 229;
+            break;
+          case '6':
+            keycode = 230;
+            break;
+          case '7':
+            keycode = 231;
+            break;
+          case '8':
+            keycode = 232;
+            break;
+          case '9':
+            keycode = 233;
+            break;
+          case '0':
+            keycode = 234;
+            break;
         }
-        break;
-      case '1':
-        key = 225;
-        break;
-      case '2':
-        key = 226;
-        break;
-      case '3':
-        key = 227;
-        break;
-      case '4':
-        key = 228;
-        break;
-      case '5':
-        key = 229;
-        break;
-      case '6':
-        key = 230;
-        break;
-      case '7':
-        key = 231;
-        break;
-      case '8':
-        key = 232;
-        break;
-      case '9':
-        key = 233;
-        break;
-      case '0':
-        key = 234;
-        break;
-    }
 
-    //if ((e.bit.KEY >= 48 && e.bit.KEY <= 57) || e.bit.KEY == 35 || e.bit.KEY == 42) {
-    if (key > 0) {
-      if (e.bit.EVENT == KEY_JUST_PRESSED) {
-        //Keyboard.write(e.bit.KEY);
-        //if (key != lastKeyPressed) {
-          Keyboard.press(key);          
-       // }
-        lastKeyPressed = key;
-      } else if (e.bit.EVENT == KEY_JUST_RELEASED) {
-        Keyboard.release(key);
-        //lastKeyPressed = 0;
+        switch (keypad.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
+          case PRESSED:
+            if (key == '#' && (isHeld('*') || isPressed('*'))) {
+              keycode = 224;
+              Keyboard.press(keycode);
+              delay(100);
+              Keyboard.releaseAll();
+            } else if (key == '6' && (isHeld('*') || isPressed('*'))) {
+              //*6 will toggle numlock mode
+              keycode = 219;
+              Keyboard.press(keycode);
+              delay(100);
+              Keyboard.releaseAll();
+            } else {
+              Keyboard.press(keycode);
+            }
+            break;
+          case HOLD:
+            break;
+          case RELEASED:
+            Keyboard.release(keycode);
+            break;
+          case IDLE:
+            break;
+        }
       }
-    }    
+    }
   }
-
-  delay(10);
-}
+}  // End loop
