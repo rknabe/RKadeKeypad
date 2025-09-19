9b246f13040abdb14a27fd7adaf2d06a557a90d0
rknabe <rusty.knabe@gmail.com>
2025-09-19 13:57:46 -0500

    use key hold instead of combos

--- RKadeKeypad.ino
+++ RKadeKeypad.ino
@@ -12,9 +12,11 @@ char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
 byte rowPins[KEYPAD_ROWS] = { 1, 6, 5, 3 };  //connect to the row pinouts of the kpd
 byte colPins[KEYPAD_COLS] = { 2, 0, 4 };     //connect to the column pinouts of the kpd
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
+char lastKeyHeld = ' ';
 
 void setup() {
   BootKeyboard.begin();
+  keypad.setHoldTime(1000);
 }
 
 void processKeypad() {
@@ -38,24 +40,29 @@ void processKeypad() {
             break;
         }
 
-        switch (keypad.key[i].kstate) {  // Report active key state : IDLE, PRESSED, HOLD, or RELEASED
+        switch (keypad.key[i].kstate) {
           case PRESSED:
-            if (key == '#' && (isHeld('*') || isPressed('*'))) {
-              BootKeyboard.write(KEYPAD_ENTER);
-            } else if (key == '6' && (isHeld('*') || isPressed('*'))) {
-              //*6 will toggle numlock mode
+            if (!BootKeyboard.getLeds() & LED_NUM_LOCK) {
               BootKeyboard.write(KEY_NUM_LOCK);
-            } else {
-              if (!BootKeyboard.getLeds() & LED_NUM_LOCK) {
-                BootKeyboard.write(KEY_NUM_LOCK);
-              }
-              BootKeyboard.press(KeyboardKeycode(keycode));
             }
             break;
           case HOLD:
+            if (key == '#') {
+              lastKeyHeld = '#';
+              BootKeyboard.write(KEYPAD_ENTER);
+            } else if (key == '*') {
+              lastKeyHeld = '*';
+              BootKeyboard.write(KEYPAD_DIVIDE);
+            } else if (key == '6') {
+              lastKeyHeld = '6';
+              BootKeyboard.write(KEY_NUM_LOCK);
+            }
             break;
           case RELEASED:
-            BootKeyboard.release(KeyboardKeycode(keycode));
+            if (key != lastKeyHeld) {
+              BootKeyboard.write(KeyboardKeycode(keycode));
+            }
+            lastKeyHeld = ' ';
             break;
           case IDLE:
             break;
@@ -63,26 +70,6 @@ void processKeypad() {
       }
     }
   }
-}
-
-bool isHeld(char keyChar) {
-  for (byte i = 0; i < LIST_MAX; i++) {
-    if (keypad.key[i].kchar == keyChar) {
-      if ((keypad.key[i].kstate == HOLD))
-        return true;
-    }
-  }
-  return false;  // Not held.
-}
-
-bool isPressed(char keyChar) {
-  for (byte i = 0; i < LIST_MAX; i++) {
-    if (keypad.key[i].kchar == keyChar) {
-      if ((keypad.key[i].kstate == PRESSED))
-        return true;
-    }
-  }
-  return false;  // Not pressed.
 }
 
 void loop() {
