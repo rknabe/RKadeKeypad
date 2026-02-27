cac64cdac2cb9d9d36be030ea47b082adfc6db10
Rusty Knabe <rusty.knabe@gmail.com>
2026-02-26 18:51:01 -0600

    support motogp keypad

--- RKadeKeypad.ino
+++ RKadeKeypad.ino
@@ -1,5 +1,6 @@
 #include <Keypad.h>
 #include <HID-Project.h>
+#include <EEPROM.h>
 
 const byte KEYPAD_ROWS = 4;  //four rows
 const byte KEYPAD_COLS = 3;  //three columns
@@ -9,14 +10,58 @@ char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
   { '7', '8', '9' },
   { '*', '0', '#' }
 };
-byte rowPins[KEYPAD_ROWS] = { 1, 6, 5, 3 };  //connect to the row pinouts of the kpd
-byte colPins[KEYPAD_COLS] = { 2, 0, 4 };     //connect to the column pinouts of the kpd
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
+
+struct Config {
+  byte rowPin1;
+  byte rowPin2;
+  byte rowPin3;
+  byte rowPin4;
+  byte colPin1;
+  byte colPin2;
+  byte colPin3;
+};
+
+//byte rowPins[KEYPAD_ROWS] = { 1, 6, 5, 3 };  //connect to the row pinouts of the kpd
+//byte colPins[KEYPAD_COLS] = { 2, 0, 4 };     //connect to the column pinouts of the kpd
+byte rowPins[KEYPAD_ROWS];  //connect to the row pinouts of the motogp kpd
+byte colPins[KEYPAD_COLS];  //connect to the column pinouts of the motogp kpd
+void (*resetFunc)(void) = 0;
 char lastKeyHeld = ' ';
 bool numPadMode = true;
 #define SERIAL_BAUDRATE 9600
-#define VERSION "1.0.1"
+#define VERSION "1.0.2"
 #define HOLD_TIME 1100
+
+bool configValid(Config config) {
+  return config.colPin1 + config.colPin2 + config.colPin3 + config.rowPin1 + config.rowPin2 + config.rowPin3 + config.rowPin4 == 21;
+}
+
+Keypad setupKeypad() {
+  Config config;  //Variable to store custom object read from EEPROM.
+  EEPROM.get(0, config);
+  if (!configValid(config)) {
+    //defaults
+    config.colPin1 = 2;
+    config.colPin2 = 0;
+    config.colPin3 = 4;
+    config.rowPin1 = 1;
+    config.rowPin2 = 6;
+    config.rowPin3 = 5;
+    config.rowPin4 = 3;
+    EEPROM.put(0, config);
+  }
+  colPins[0] = config.colPin1;
+  colPins[1] = config.colPin2;
+  colPins[2] = config.colPin3;
+  rowPins[0] = config.rowPin1;
+  rowPins[1] = config.rowPin2;
+  rowPins[2] = config.rowPin3;
+  rowPins[3] = config.rowPin4;
+  return Keypad(makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS);
+}
+
+
+Keypad keypad = setupKeypad();
 
 void setup() {
   Serial.begin(SERIAL_BAUDRATE);
@@ -26,7 +71,7 @@ void setup() {
   keypad.setHoldTime(HOLD_TIME);
   numPadMode = true;
 
-  //this help firmware update
+  //this helps firmware update
   delay(500);
 }
 
@@ -147,6 +192,30 @@ void processSerial() {
     } else if (strcmp_P(cmd, PSTR("version")) == 0) {
       Serial.print("rkade keypad version:");
       Serial.println(VERSION);
+    } else if (strcmp_P(cmd, PSTR("setTypeMotoGp")) == 0) {
+      Config config;
+      config.colPin1 = 3;
+      config.colPin2 = 2;
+      config.colPin3 = 4;
+      config.rowPin1 = 0;
+      config.rowPin2 = 1;
+      config.rowPin3 = 6;
+      config.rowPin4 = 5;
+      EEPROM.put(0, config);
+      Serial.println("Saved setting for motogp type");
+      resetFunc();
+    } else if (strcmp_P(cmd, PSTR("setTypeDefault")) == 0) {
+      Config config;
+      config.colPin1 = 2;
+      config.colPin2 = 0;
+      config.colPin3 = 4;
+      config.rowPin1 = 1;
+      config.rowPin2 = 6;
+      config.rowPin3 = 5;
+      config.rowPin4 = 3;
+      EEPROM.put(0, config);
+      Serial.println("Saved setting for default type");
+      resetFunc();
     }
   }
 }
